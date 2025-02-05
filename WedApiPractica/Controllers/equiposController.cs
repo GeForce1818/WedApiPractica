107948a821cd53255866b21c561d9814748f4e19
Jorge_castro1 <jorge.castro1@catolica.edu.sv>
2025-02-04 22:58:40 -0600

    Agregar archivos de proyecto.

---
+++ WedApiPractica/Controllers/equiposController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webApiPractica.Models;
+using Microsoft.EntityFrameworkCore;
+using WedApiPractica.Models;
+
+namespace WedApiPractica.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class equiposController : ControllerBase
+    {
+        private readonly equiposContex _equiposContexto;
+
+        public equiposController (equiposContex equipoContexto)
+        {
+            this._equiposContexto = equipoContexto;
+        }
+        /// <summary>
+        /// EndPoint que retorna el listado de todos los equipos existentes
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Get()
+        {
+            List<equipos> listadoEquipo = (from e in _equiposContexto.equipos
+                                           select e).ToList();
+
+            if (listadoEquipo.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(listadoEquipo);
+        }
+
+    }
+
+}
+
---
+++ WedApiPractica/Models/equiposContex.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using webApiPractica.Models;
+
+namespace WedApiPractica.Models
+{
+    public class equiposContex : DbContext
+    {
+        public equiposContex (DbContextOptions<equiposContex> options) : base(options) 
+        { 
+        
+        }
+        public DbSet<equipos> equipos { get; set; }
+    }
+}
